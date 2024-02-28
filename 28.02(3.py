e0bdf9c39cf76bcd8d6a4daf12c346c8e81cc9c2
WHappy2 <f20052301roman@gmail.com>
2024-02-28 14:41:27 +0300

    Федоров Роман 2 пара

--- 28.02(3.py
+++ 28.02(3.py
@@ -1,5 +1,9 @@
-word = input('Введите слово:').lower()
-if 'ф' in word:
-    print('Ого! Это редкое слово!', word)
-else:
-    print('Эх, это не очень редкое слово...', word)
+
+while True:
+    word = input('Введите слово:').lower()
+    if 'ф' in word:
+        print('Ого! Это редкое слово!', word)
+        break
+    else:
+        print('Эх, это не очень редкое слово...', word)
+
